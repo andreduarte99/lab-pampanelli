e9e6195c75bb3d393f29dbbcb50c72f27885920c
andreduarte99 <andre.duarte16@outlook.com>
2024-06-30 14:46:28 -0300

    botões página sobre

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*Mostrar e Esconder carrosel */
 document.getElementById('menu').addEventListener('change', function() {
     var carrosel = document.getElementById('carrosel');
     if (this.checked) {
@@ -8,6 +10,7 @@ document.getElementById('menu').addEventListener('change', function() {
 });
 
 
+/*Painel fale conosco*/
 function openOverlay() {
     document.getElementById('overlay').style.display = 'flex';
 }
@@ -15,3 +18,43 @@ function openOverlay() {
 function closeOverlay(){
     document.getElementById('overlay').style.display = 'none';
 }
+
+/*Sessão sobre */
+
+document.getElementById('mostrarDivButtonSobre').addEventListener('click', function(){
+    var contentDiv = document.getElementById('contentDivSobre');
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+});
+
+document.getElementById('mostrarDivButtonMissao').addEventListener('click', function(){
+    var contentDiv = document.getElementById('contentDivMissao');
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+});
+
+document.getElementById('mostrarDivButtonVisao').addEventListener('click', function(){
+    var contentDiv = document.getElementById('contentDivVisao');
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+});
+
+document.getElementById('mostrarDivButtonValores').addEventListener('click', function(){
+    var contentDiv = document.getElementById('contentDivValores');
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+});
+
+
