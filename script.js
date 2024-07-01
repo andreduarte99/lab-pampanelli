23b376777e1927e20a3d4721abe5c2033f5103ac
andreduarte99 <andre.duarte16@outlook.com>
2024-06-30 21:55:56 -0300

    terminando os botões sobre

--- script.js
+++ script.js
@@ -21,40 +21,57 @@ function closeOverlay(){
 
 /*Sessão sobre */
 
+
 document.getElementById('mostrarDivButtonSobre').addEventListener('click', function(){
     var contentDiv = document.getElementById('contentDivSobre');
+    const imagem = document.getElementById('img-sobre');
     if (contentDiv.classList.contains('hidden')) {
         contentDiv.classList.remove('hidden');
+        imagem.src = './img/remover.png';
     } else {
         contentDiv.classList.add('hidden');
+        imagem.src = './img/botao-adicionar.png';
     }
+
 });
 
 document.getElementById('mostrarDivButtonMissao').addEventListener('click', function(){
     var contentDiv = document.getElementById('contentDivMissao');
+    const imagem = document.getElementById('img-missao');
     if (contentDiv.classList.contains('hidden')) {
         contentDiv.classList.remove('hidden');
+        imagem.src = './img/remover.png';
     } else {
         contentDiv.classList.add('hidden');
+        imagem.src = './img/botao-adicionar.png';
     }
 });
 
 document.getElementById('mostrarDivButtonVisao').addEventListener('click', function(){
     var contentDiv = document.getElementById('contentDivVisao');
+    const imagem = document.getElementById('img-visao');
+
     if (contentDiv.classList.contains('hidden')) {
         contentDiv.classList.remove('hidden');
+        imagem.src = './img/remover.png';
     } else {
         contentDiv.classList.add('hidden');
+        imagem.src = './img/botao-adicionar.png';
     }
 });
 
 document.getElementById('mostrarDivButtonValores').addEventListener('click', function(){
     var contentDiv = document.getElementById('contentDivValores');
+    const imagem = document.getElementById('img-valores');
     if (contentDiv.classList.contains('hidden')) {
         contentDiv.classList.remove('hidden');
+        imagem.src = './img/remover.png';
     } else {
         contentDiv.classList.add('hidden');
+        imagem.src = './img/botao-adicionar.png';
     }
 });
+
+
 
 
